79bf58f5d83eba3ef3d66072a906f61c29c6d37f
Vincent <vincentlin0803@gmail.com>
2023-02-23 16:53:07 -0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Bootcamp Project Spring 2023!!!</title>
+        <title>TODO: </title>
       </Head>
       <Stack p={4}>
-        <Heading>WDB Next.js Template</Heading>
+        <Heading>Bootcamp Project Spring 2023!!!</Heading>
         <Box>
           <Button variant="blue" onClick={() => setChild({})}>
             {(child as ReactNode) ?? "Test error handling"}
